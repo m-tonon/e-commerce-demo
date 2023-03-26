089a8e71ba59ceb9bb3da64ebbae07e944c1f9f1
m-tonon <math.tonon@gmail.com>
2023-03-26 07:31:53 -0300

    feat: create and styling home & hero component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeroComponent } from './pages/home/components/hero/hero.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    HeroComponent,
+
   ],
   imports: [
     BrowserModule,
