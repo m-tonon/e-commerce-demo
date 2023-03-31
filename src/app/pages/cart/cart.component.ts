123a92193ce24467daa4452df37dbe652698be51
m-tonon <math.tonon@gmail.com>
2023-03-31 07:56:19 -0300

    feat: add cart table

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Cart, CartItem } from 'src/app/models/cart.model';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class CartComponent {
 
+export class CartComponent implements OnInit {
+  cart: Cart = {
+    items: [
+      {
+        product: '../../../../../../assets/images/products/women/product4.jpg',
+        name: 'womens skirt',
+        price: 150,
+        quantity: 1,
+        id: 1,
+      },
+    ],
+  };
+  dataSource: Array<CartItem> = [];
+  displayedColumns: Array<string> = [
+    'Product',
+    'Name',
+    'Price',
+    'Quantity',
+    'Total',
+    ''
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.dataSource = this.cart.items;
+  }
 }
