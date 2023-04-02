bcea29a06e98290a43efa8f0e1cb87ecf53c325b
m-tonon <math.tonon@gmail.com>
2023-04-02 07:12:19 -0300

    feat: add and remove quantity for cart item

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -62,4 +62,12 @@ export class CartComponent implements OnInit {
   onRemoveFromCart(item: CartItem): void {
     this.cartService.removeFromCart(item);
   }
+
+  onAddQuantity(item: CartItem): void {
+    this.cartService.addToCart(item);
+  }
+
+  onRemoveQuantity(item: CartItem): void {
+    this.cartService.removeQuantity(item);
+  }
 }
