52bd0f28bd9df0ba23f8c3edd67f18dc52f5f61a
m-tonon <math.tonon@gmail.com>
2023-04-05 06:34:23 -0300

    feat: fetching collection from home page with fakeapi store

--- src/app/pages/home/components/collections/collections.component.ts
+++ src/app/pages/home/components/collections/collections.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription, switchMap } from 'rxjs';
+import { Product } from 'src/app/models/product.model';
+import { StoreService } from 'src/app/services/store.service';
 
 @Component({
   selector: 'app-collections',
@@ -6,6 +9,33 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class CollectionsComponent {
 
+export class CollectionsComponent implements OnInit, OnDestroy {
+  menProducts?: Array<Product>;
+  womenProducts?: Array<Product>;
+  menProductsSubs?: Subscription;
+  womenProductsSubs?: Subscription;
+
+  constructor(private storeService: StoreService) {}
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts(): void {
+      this.menProductsSubs = this.storeService.getAllProducts('3','desc', 'men\'s clothing').
+      subscribe((_product) => {
+        this.menProducts = _product;
+      });
+
+    this.womenProductsSubs = this.storeService.getAllProducts('3','desc', 'women\'s clothing').
+      subscribe((_product) => {
+        this.womenProducts = _product;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.menProductsSubs?.unsubscribe();
+    this.womenProductsSubs?.unsubscribe();
+  }
 }
