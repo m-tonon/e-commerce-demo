1e60295bc074f14108b4f6aef0c0ad08135ecc5c
m-tonon <math.tonon@gmail.com>
2023-04-19 07:33:25 -0300

    feat: add auto login to keep user when app is loaded

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cart } from './models/cart.model';
 import { CartService } from './services/cart.service';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -10,11 +11,14 @@ import { CartService } from './services/cart.service';
 export class AppComponent implements OnInit {
   cart: Cart = { items: [] };
 
-  constructor (private cartService: CartService) {}
+  constructor (
+    private cartService: CartService,
+    private authService: AuthService) {}
 
   ngOnInit() {
     this.cartService.cart.subscribe((_cart) => {
       this.cart = _cart;
     });
+    this.authService.autologin();
   }
 }
