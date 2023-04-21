01813f661e8ba0c9b25005d6b230a336c5fa81b2
m-tonon <math.tonon@gmail.com>
2023-04-21 07:09:32 -0300

    refactor: organizing an auth folder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cart } from './models/cart.model';
 import { CartService } from './services/cart.service';
-import { AuthService } from './services/auth.service';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
